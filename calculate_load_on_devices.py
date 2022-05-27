dec1535b7dac312b5ec5133ae57fe47f116580e0
Paweł Marszał <marszal.pawel@yahoo.com>
2022-05-27 19:48:07 +0200

    Migrate to pandas and use correct VM name

--- calculate_load_on_devices.py
+++ calculate_load_on_devices.py
@@ -1,28 +1,15 @@
 from numpy import int16
 
-def main(src, dest):
-    import numpy as np
+def aggregate_load_data_from_devices(src, dst):
+    import pandas as pd
 
-    data = np.genfromtxt(src, delimiter=',', usecols=(7, 2), skip_header=1, dtype=['U15', np.uint16])
+    data = pd.read_csv(src, usecols=['DestinationVmName', 'Iteration'])[['DestinationVmName', 'Iteration']]
+    aggregated = data.groupby(['DestinationVmName', 'Iteration']).size().reset_index(name='Count')
 
-    count_unique = {}
-    for row in data:
-        name = row[0]
-        t = (str(row[0]), int(row[1]))
-        if t not in count_unique:
-            count_unique[t] = 0
-        count_unique[t] += 1
+    aggregated.to_csv(dst, index=False)
 
-    result = []
-    for device, iter in count_unique.keys():
-        count = count_unique[(device, iter)]
-        result.append([device, iter, count])
-    
-    result = np.array(result)
-    result = result[result[:, 1].argsort(kind='stable')]
-    result = result[result[:, 0].argsort(kind='stable')]
-
-    np.savetxt(dest, result, fmt=['%s','%s','%s'], delimiter=',', comments='', header='IoTDeviceName,Iteration,Count')
+def main(src, dst):
+    aggregate_load_data_from_devices(src, dst)
 
 if __name__ == '__main__':
     import sys
@@ -32,6 +19,6 @@ if __name__ == '__main__':
         print(f'\tpython {sys.argv[0]} <src-filename>.csv <dst-filename>.csv')
         sys.exit('')
     
-    source = f'osmosis_csv/{sys.argv[1]}'
-    dest = f'outputStats/{sys.argv[2]}'
+    source = sys.argv[1]
+    dest = sys.argv[2]
     main(source, dest)
